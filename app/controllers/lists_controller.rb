e43999e0710cd188b2bb817738675d880436a38e
Alex Kaufman <alexkaufman06@gmail.com>
2015-03-15 11:45:10 -0700

    Add implicit rendering, bootstrap, and alert messages

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,17 +1,16 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
-    render :index
   end
 
   def new
     @list = List.new
-    render :new
   end
 
   def create
     @list = List.new(params[:list])
     if @list.save
+      flash[:notice] = "List successfully saved!"
       redirect_to lists_path
     else
       render :new
@@ -20,17 +19,16 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
-    render :show
   end
 
   def edit
     @list = List.find(params[:id])
-    render :edit
   end
 
   def update
     @list = List.find(params[:id])
     if @list.update(params[:list])
+      flash[:notice] = "List successfully updated!"
       redirect_to lists_path
     else
       render :edit
@@ -39,7 +37,9 @@ class ListsController < ApplicationController
 
   def destroy
     @list = List.find(params[:id])
-    @list.destroy
+    if @list.destroy
+      flash[:alert] = "List successfully deleted!"
+    end
     redirect_to lists_path
   end
 end
